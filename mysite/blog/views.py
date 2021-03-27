1875761a84fe8d00316f4f3fd7a4e7daa4aa9288
bartoszblazewicz <morthell@gmail.com>
2021-03-27 15:50:44 +0100

    share via mail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import  Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
-
+#widok oparty na klasie
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
@@ -39,4 +41,29 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+##Formularz wysylania emaila
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status = 'published')
+    sent = False
+
+    if request.method == 'POST':
+        #formularz zostal wyslany
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #Weryfikacja pol formularza zakonczyla sie powodzeniem
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) zacheca do przeczytania "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Przeczytaj post "{}" na stronie {}\n\n Komentarz dodany przez {}:{}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'morthell@gmail.com', [cd['to']])
+            sent = True
+            #mozna wyslac wiadomosc email
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
